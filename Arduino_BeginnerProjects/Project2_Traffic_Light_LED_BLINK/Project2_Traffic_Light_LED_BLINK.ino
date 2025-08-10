aa05b6160de2651ad1a0ca99cc4e94faa2b501fc
MuMuDevOn <mutsaaayisha05@gmail.com>
2025-08-10 06:29:20 +0200

    Updated program comments

--- Arduino_BeginnerProjects/Project2_Traffic_Light_LED_BLINK/Project2_Traffic_Light_LED_BLINK.ino
+++ Arduino_BeginnerProjects/Project2_Traffic_Light_LED_BLINK/Project2_Traffic_Light_LED_BLINK.ino
@@ -11,15 +11,15 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(7, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(5000);                      // wait for a second
-  digitalWrite(7, LOW); // turn the LED off by making the voltage LOW
-  digitalWrite(5, HIGH);
-  delay(1000);
-   digitalWrite(5, LOW);   
-  digitalWrite(3, HIGH); // wait for a second
-  delay(5000); 
-  digitalWrite(3, LOW); 
+digitalWrite(7, HIGH);  // turn the LED on (HIGH is the voltage level)(Red led light will be on)
+  delay(5000);                      // wait for 5 second
+  digitalWrite(7, LOW); // turn the red LED off by making the voltage LOW
+  digitalWrite(5, HIGH);//Switch the yellow LED
+  delay(1000); //wait for 1 second
+   digitalWrite(5, LOW);   //Switch of yellow LED
+  digitalWrite(3, HIGH); // Turn on Green LED Lights 
+  delay(5000); //Wait(Delay) for 5 seconds
+  digitalWrite(3, LOW); //Switch of the Green LED lights
 
 }
 
